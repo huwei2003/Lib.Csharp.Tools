51658970d758edcf0b659abd1d53108f08723559
jack <jack@163.com>
2016-01-08 18:24:43 +0800

    add jsonhelper test

---
+++ Lib.Csharp.Tools/Lib.Csharp.ToolsTests/JsonHelperTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Lib.Csharp.ToolsTests
+{
+    [TestFixture()]
+    public class JsonHelperTests
+    {
+        /// <summary>
+        /// 全局setup,不能使用async
+        /// </summary>
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+
+        }
+        /// <summary>
+        /// 每次测试setup,不能使用async
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test()]
+        public void ListToJsonTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void ListToJsonTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void ToJsonOldTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void ToJsonTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void ToObjectTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void ToListObjectTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void ToJsonTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void ToJsonTest2()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void ToJsonTest3()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void ToJsonTest4()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void String2JsonTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lib.Csharp.Tools/Lib.Csharp.ToolsTests/SmtpMailHelperTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Lib.Csharp.Tools;
+using NUnit.Framework;
+
+namespace Lib.Csharp.ToolsTests
+{
+    [TestFixture()]
+    public class SmtpMailHelperTests
+    {
+        /// <summary>
+        /// 全局setup,不能使用async
+        /// </summary>
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+
+        }
+        /// <summary>
+        /// 每次测试setup,不能使用async
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test()]
+        [ExpectedException(typeof(Exception))]
+        public async void SendMailTest_Noinit_Exception()
+        {
+            var mail = new MailData()
+            {
+                Attachments="",
+                Content="邮件测试啦",
+                MailTextType = MediaTextType.Text,
+                Subject="test mail",
+                To = "******@qq.com"
+            };
+            var result = await SmtpMailHelper.SendMail(mail);
+        }
+
+        [Test()]
+        public async void SendMailTest_Success()
+        {
+            var mail = new MailData()
+            {
+                Attachments = "",
+                Content = "邮件测试啦",
+                MailTextType = MediaTextType.Text,
+                Subject = "test mail",
+                To = "****@qq.com"
+            };
+            var config = new SmtpMailConfig()
+            {
+                FromEail="******@163.com",
+                FromName="nickname",
+                Password="password",
+                ReplyEmail = "*****@163.com",
+                SmsCanUse="true",
+                SmtpHost="smtp.163.com",
+                SmtpPort=25,
+            };
+            SmtpMailHelper.Init(config);
+
+            var result = await SmtpMailHelper.SendMail(mail);
+
+            Assert.AreEqual(result, true);
+        }
+    }
+}
